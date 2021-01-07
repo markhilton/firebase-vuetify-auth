0d43e0ee0124b84be8504367e4769c47bf4d82ab
Mark Hilton <nerd305@gmail.com>
2021-01-07 12:10:04 -0500

    working latest version

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs" // Convert CommonJS modules to ES6
+import vue from "rollup-plugin-vue" // Handle .vue SFC files
 import postcss from "rollup-plugin-postcss"
 import vuetify from "rollup-plugin-vuetify"
 import buble from "@rollup/plugin-buble" // Transpile/polyfill with reasonable browser support
@@ -20,7 +21,13 @@ export default {
     commonjs({
       include: "node_modules/**",
     }),
-    postcss(),
+    vue({
+      css: true, // Dynamically inject css as a <style> tag
+      compileTemplate: true, // Explicitly convert template to render function
+    }),
+    postcss({
+      plugins: [],
+    }),
     vuetify(),
     buble({
       transforms: { asyncAwait: false },
