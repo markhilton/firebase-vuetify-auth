4429bc4f53a34d662ec5cef2400da650afa4bfe5
Mark Hilton <nerd305@gmail.com>
2021-01-06 19:37:12 -0500

    lint

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs" // Convert CommonJS modules to ES6
-import vue from "rollup-plugin-vue" // Handle .vue SFC files
 import postcss from "rollup-plugin-postcss"
 import vuetify from "rollup-plugin-vuetify"
 import buble from "@rollup/plugin-buble" // Transpile/polyfill with reasonable browser support
@@ -20,10 +19,6 @@ export default {
     resolve(),
     commonjs({
       include: "node_modules/**",
-    }),
-    vue({
-      css: true, // Dynamically inject css as a <style> tag
-      compileTemplate: true, // Explicitly convert template to render function
     }),
     postcss(),
     vuetify(),
