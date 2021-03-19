33601a9ae5a902c0112ed9ff55521ac1593f7cc9
Mark Hilton <nerd305@gmail.com>
2021-03-19 17:10:36 -0400

    debug test

--- src/components/authentication/debug.js
+++ src/components/authentication/debug.js
@@ -2,6 +2,8 @@ import Vue from "vue"
 
 // https://stackoverflow.com/questions/4116608/pass-unknown-number-of-arguments-into-javascript-function
 export default (...text) => {
+  console.log("DEBUG", Vue.prototype.$authGuardSettings)
+
   if (!Boolean(Vue.prototype.$authGuardSettings.debug)) return
 
   console.log.apply(this, text)
