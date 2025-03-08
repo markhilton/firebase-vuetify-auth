90bb8f9541da74fcc6fb2187af39f96c1e126aeb
Mark Hilton <nerd305@gmail.com>
2025-03-08 16:57:36 -0500

    fix settings

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -2,8 +2,7 @@ import router from "@/router"
 import { app as firebase } from "@/middleware/firebase"
 import AuthGuard from "../wrapper"
 
-// Configure AuthGuard with settings
-AuthGuard.init({
+const authGuardSettings = {
   debug: true,
   session: "local", // or "browser" see: https://firebase.google.com/docs/auth/web/auth-state-persistence
   router,
@@ -22,6 +21,6 @@ AuthGuard.init({
   subtitle: "Firebase Auth Demo for Veutify",
   icon: "mdi-test-tube",
   iconColor: "#FF6D00",
-})
+}
 
-export { AuthGuard }
+export { AuthGuard, authGuardSettings }
